f66077e372730eae4352310bf62bc12f815e3be9
Vicente Pacheco <vicepachecoa@gmail.com>
2025-12-18 16:04:34 -0300

    back inicio

---
+++ Level-Up-master/level-up/src/main/kotlin/level_up/LevelUpApplication.kt
@@ -0,0 +1,11 @@
+package level_up
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LevelUpApplication
+
+fun main(args: Array<String>) {
+	runApplication<LevelUpApplication>(*args)
+}
---
+++ Level-Up-master/level-up/src/test/kotlin/level_up/LevelUpApplicationTests.kt
@@ -0,0 +1,13 @@
+package level_up
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LevelUpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
